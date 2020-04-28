1c99c8eae4801eaf06ccbd574bad59e007fe3b16
David <mustermannmuster2@gmail.com>
2020-04-28 16:46:33 +0200

    Style the app with bootstrap and css. Also, apply DRY principle by using extensions in Django.

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,10 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blog/post_list.html', context)
+    stuff_for_frontend = {'posts': posts}
+    return render(request, 'blog/post_list.html', stuff_for_frontend)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    context = {'post': post}
-    return render(request, 'blog/post_detail.html', context)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blog/post_detail.html', stuff_for_frontend)
